5955d864741c793673112e18ce675a6245454612
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-11 22:07:19 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,20 +1,33 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-      #your key/value pairs here!
+  def first_challenge
+    epic_tragedy = {
+      montague: {
+        patriarch: {
+
+        },
+        matriarch: {
+
+        },
+        hero: {
+
+        },
+        hero_friends: []
       },
-    :capulet => {
-      #your key/value pairs here!
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
+      capulet: {
+        patriarch: {
+
+        },
+        matriarch: {
+
+          } ,
+        heroine: {
+
+        },
+        heroine_friends: []
+
       }
     }
-
-
+  end
 end
+
+
